0500b4b7e5c0767a6e7787ed39aec831ff25a4b2
fbuggia1 <71612806+fbuggia1@users.noreply.github.com>
2021-06-24 16:10:43 -0400

    basic GPT-3 Functionality
    Calls openai API, gets output, saves input, output and date logged into database. Cleaned up some of the Django organization.

--- backend/frontend/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from frontend import views
-
-urlpatterns = [
-    path('', views.home_view, name='home'),
-]
--- backend/frontend/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def home_view(request):
-	#print(request.GET)
-	return render(request,"home.html")
