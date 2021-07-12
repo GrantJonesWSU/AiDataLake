8fa0094941d5f37bef1025b87daa44f9ff7852b5
RubelMohammed <84865502+RubelMohammed@users.noreply.github.com>
2021-07-12 13:48:17 -0400

    Add files via upload
    Mohammed Rubel
    Lines changed:
    gpt3.py  -- Line example 
    views.py -- Line
    home.html -- Line
    Don't upload now, ...h okow can I make new branch...new repository? yea or a new branch? New branch

---
+++ views.py
@@ -0,0 +1,91 @@
+import re
+import requests
+from django.http import request
+from django.shortcuts import render
+from django.utils.timezone import datetime
+from frontend.models import GptInputOutput
+from frontend.models import GptIOManager
+from frontend.models import UserDatabase
+from frontend.models import UserDatabaseManager
+from frontend.models import UserDatabaseEntity
+from frontend.models import UserDatabaseEntityManager
+from frontend.gpt3 import GetGptResponse
+from frontend.gpt3 import TrainGptInputGeneric
+from frontend.gpt3 import TrainGptInputSql
+
+
+# Create your views here.
+def home_view(request):
+	return render(request,"home.html")
+
+def instruction_view(request):
+	return render(request,"instructions.html")
+
+#Handles User Login
+def user_login(request):
+	# needs to render the login page and then upon login
+	# redirect to the home page
+	return render(request, "home.html")
+
+#Handles DB Schema File
+def file_upload(request):
+	return render(request,"home.html")
+
+#Handles User History Query
+def user_history(request):
+	userHistory = GptInputOutput.objects.all()
+	# fetch the history and then send to page render
+
+	return render(request,"home.html", {"user_history_list" : userHistory})
+
+#Handles Recent Meta Query
+def recent_meta(request):
+	recentMeta = "no recent meta to show or encountered error"
+
+	return render(request,"home.html", {"recent_meta_output" : recentMeta})
+
+def gpt_view(request):
+	gptOutput = "none, may have been an error or bug"
+
+	if request.method == "POST":
+		#Create Dict of Request
+		readRequest=request.POST
+
+		#Store Query String
+		queryString=readRequest["genericGptInput"]
+
+		#My update
+		SQLVersion = readRequest["SQLFlavor"]
+															#My Updates
+		trainedInput = TrainGptInputSql(queryString, 1, SQLVersion)
+		gptOutput = GetGptResponse(trainedInput)
+		# call convert service
+			#My Updated
+		# response = requests.post("http://www.sqlines.com/sqlines_run.php", data = {'source_type':'SQL','target_type':'Microsoft SQL Server','source':gptOutput})
+		# print(response.status_code)
+		# print(response.content)
+
+		# still need to save to DB
+		gptObject = GptInputOutput.objects.createGptIO(queryString, trainedInput, gptOutput, datetime.now())
+		gptObject.save()
+
+	return render(request,"home.html", {"gpt_output" : gptOutput})
+	
+def gpt_sql_view(request):
+	gptOutput = "none, may have been an error or bug"
+
+	if request.method == "POST":
+		#Create Dict of Request
+		readRequest=request.POST
+
+		#Store Query String
+		queryString=readRequest["sqlGptInput"]
+
+		trainedInput = TrainGptInputGeneric(queryString, 1)
+		gptOutput = str(GetGptResponse(trainedInput))
+
+		# still need to save to DB
+		gptObject = GptInputOutput.objects.createGptIO(queryString, trainedInput, gptOutput, datetime.now())
+		gptObject.save()
+	
+	return render(request,"home.html", {"gpt_output" : gptOutput})
