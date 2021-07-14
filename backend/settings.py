64de645107a099c2a931f11398b0975246729f77
gf2096 <62407422+gf2096@users.noreply.github.com>
2021-07-14 08:53:35 -0400

    database name changes
    database name chnged

--- backend/settings.py
+++ backend/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
    'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'id16988493_amazondatalake',                      # Or path to database file if using sqlite3.
+        'NAME': 'datalibrarianapplication',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
