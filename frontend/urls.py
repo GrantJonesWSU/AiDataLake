9d571cf67cc77f9caf5ee3746253e998fc352a1b
rubel <rubel@email>
2021-07-30 02:43:24 -0400

    style improvements

--- frontend/urls.py
+++ frontend/urls.py
@@ -8,7 +8,7 @@ from frontend import gpt3
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('instructions', views.instruction_view, name='readMe'),
-    path('userLogin', views.user_login, name='userLogin'),
+    # path('userLogin', views.user_login, name='userLogin'),
     path('fileUpload', testAI.file_upload, name='dbfile'),
     path('userHistory', views.user_history, name='userhist'),
     path('recentMeta', views.recent_meta, name='recentMeta'),
