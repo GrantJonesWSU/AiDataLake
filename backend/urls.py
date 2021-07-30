b32999fc9fd768835986475d5db4870fdf6396fd
gf2096 <62407422+gf2096@users.noreply.github.com>
2021-07-30 19:17:29 -0400

    Links
    added links for convenience for the account screen

--- backend/urls.py
+++ backend/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", include("frontend.urls")),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
