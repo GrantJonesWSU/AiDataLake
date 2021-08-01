f25ae0386a5666a56096ad646986322e4c452c9d
gf2096 <62407422+gf2096@users.noreply.github.com>
2021-08-01 17:33:01 -0400

    unit testing for account screen

--- frontend/test/test_authentication.py
+++ frontend/test/test_authentication.py
@@ -13,10 +13,18 @@ class BaseTest(TestCase):
 
         
         self.test_user = User.objects.create_user(
-            email="test@gmail.com",
+            email = "test@gmail.com",
             username = "TestUser", 
             password="jopkoplop",
             )
+        
+        self.test_user2 = User.objects.create_user(
+            email="test2@gmail.com",
+            username = "TestUser2", 
+            password="jopkoplop",
+            )
+
+
         return super().setUp()
 
 class RegisterTest(BaseTest):
@@ -85,6 +93,36 @@ class RegisterTest(BaseTest):
         form = NewUserForm(data)
         self.assertFalse(form.is_valid())
 
+    def test_only_password1(self):
+        data={
+            'email':'',
+            'username':'',
+            'password1':'jopkoplop',
+            'password2':'',
+            }
+        form = NewUserForm(data)
+        self.assertFalse(form.is_valid())
+    
+    def test_only_password2(self):
+        data={
+            'email':'',
+            'username':'',
+            'password1':'',
+            'password2':'jopkoplop',
+            }
+        form = NewUserForm(data)
+        self.assertFalse(form.is_valid())
+    
+    def test_only_both_password(self):
+        data={
+            'email':'',
+            'username':'',
+            'password1':'jopkoplop',
+            'password2':'jopkoplop',
+            }
+        form = NewUserForm(data)
+        self.assertFalse(form.is_valid())
+
     def test_missing_both_passwords(self):
         data={
             'email':'testemail@gmail.com',
@@ -145,6 +183,16 @@ class RegisterTest(BaseTest):
         form = NewUserForm(data)
         self.assertFalse(form.is_valid())
     
+    def test_only_username(self):
+        data={
+            'email':'',
+            'username':'TestUser',
+            'password1':'',
+            'password2':'',
+            }
+        form = NewUserForm(data)
+        self.assertFalse(form.is_valid())
+    
     def test_taken_username(self):
         data={
             'email':'testemail@gmail.com',
@@ -164,10 +212,50 @@ class RegisterTest(BaseTest):
             }
         form = NewUserForm(data)
         self.assertFalse(form.is_valid())
+    
+    def test_only_email(self):
+        data={
+            'email':'testemail@gmail.com',
+            'username':'',
+            'password1':'',
+            'password2':'',
+            }
+        form = NewUserForm(data)
+        self.assertFalse(form.is_valid())
 
     def test_taken_email(self):
         data={
             'email':'test@gmail.com',
+            'username':'username',
+            'password1':'jopkoplop',
+            'password2':'jopkoplop',
+            }
+        form = NewUserForm(data)
+        self.assertFalse(form.is_valid())
+
+    def test_invalid_email(self):
+        data={
+            'email' : "test",
+            'username':'username',
+            'password1':'jopkoplop',
+            'password2':'jopkoplop',
+            }
+        form = NewUserForm(data, instance=self.test_user)
+        self.assertFalse(form.is_valid())
+    
+    def test_invalid_email2(self):
+        data={
+            'email' : "test2@",
+            'username':'username',
+            'password1':'jopkoplop',
+            'password2':'jopkoplop',
+            }
+        form = NewUserForm(data)
+        self.assertFalse(form.is_valid())
+    
+    def test_invalid_email3(self):
+        data={
+            'email' : "test2@gmail",
             'username':'username',
             'password1':'jopkoplop',
             'password2':'jopkoplop',
@@ -197,6 +285,22 @@ class LoginTest(BaseTest):
             }
         form = AuthenticationForm(None,data)
         self.assertFalse(form.is_valid())
+
+    def test_only_username(self):
+        data={
+            'username' : "TestUser", 
+            'password' : ""
+            }
+        form = AuthenticationForm(None,data)
+        self.assertFalse(form.is_valid())
+    
+    def test_only_password(self):
+        data={
+            'username' : "", 
+            'password' : "jopkoplop"
+            }
+        form = AuthenticationForm(None,data)
+        self.assertFalse(form.is_valid())
     
     def test_wrong_username(self):
         data={
@@ -214,6 +318,91 @@ class LoginTest(BaseTest):
         form = AuthenticationForm(None,data)
         self.assertFalse(form.is_valid())
 
+
+class AccountFormTest(BaseTest):
+
+
+    def test_correct_page(self):
+        self.client.login(username='TestUser', password='jopkoplop')
+        response=self.client.get(self.account_url)
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,'account.html')
+
+    def test_valid_change_both(self):
+        data={
+            'username' : "TestUserNew", 
+            'email' : "testNew@gmail.com"
+            }
+        form = AccountUpdateForm(data, instance=self.test_user)
+        self.assertTrue(form.is_valid())
+
+
+    def test_valid_change_username(self):
+        data={
+            'username' : "TestUserNew2",
+            'email' : "test@gmail.com"
+            }
+        form = AccountUpdateForm(data, instance=self.test_user)
+        self.assertTrue(form.is_valid())
+
+    def test_valid_change_email(self):
+        data={
+            'username':"TestUser",
+            'email' : "testNew2@gmail.com"
+            }
+        form = AccountUpdateForm(data, instance=self.test_user)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_change_blank(self):
+        data={
+            'username' : ""
+            }
+        form = AccountUpdateForm(data, instance=self.test_user)
+        self.assertFalse(form.is_valid())
+
+    
+    def test_invalid_change_existing_user(self):
+        data={
+            'username' : "TestUser2", 
+            'email' : "test2@gmail.com"
+            }
+        form = AccountUpdateForm(data, instance=self.test_user)
+        self.assertFalse(form.is_valid())
+
+    def test_invalid_change_existing_user_username(self):
+        data={
+            'username' : "TestUser2"
+            }
+        form = AccountUpdateForm(data, instance=self.test_user)
+        self.assertFalse(form.is_valid())
+
+    def test_invalid_change_existing_user_email(self):
+        data={
+            'email' : "test2@gmail.com"
+            }
+        form = AccountUpdateForm(data, instance=self.test_user)
+        self.assertFalse(form.is_valid())
+
+    def test_invalid_change_invalid_email(self):
+        data={
+            'email' : "test"
+            }
+        form = AccountUpdateForm(data, instance=self.test_user)
+        self.assertFalse(form.is_valid())
+    
+    def test_invalid_change_invalid_email2(self):
+        data={
+            'email' : "test2@"
+            }
+        form = AccountUpdateForm(data, instance=self.test_user)
+        self.assertFalse(form.is_valid())
+    
+    def test_invalid_change_invalid_email3(self):
+        data={
+            'email' : "test2@gmail"
+            }
+        form = AccountUpdateForm(data, instance=self.test_user)
+        self.assertFalse(form.is_valid())
 
 
 
